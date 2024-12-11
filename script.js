e7a0e2f51c1356dc2ec4ff530a4b35765fc82698
caiocroccia22 <caiocroccia22@gmail.com>
2024-12-10 22:08:24 -0300

    feat: add JQuery validation plugin to ensure labels are not empty

--- script.js
+++ script.js
@@ -3,7 +3,39 @@ $(document).ready(function(){
 
 
 
+
     // JQuery Mask Plugin
     // Uso de mascara no campo de telefone
     $('#tel').mask('(00) 00000-0000');
+
+
+    // JQuery Validate Plugin
+    $('#contact-form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            tel: {
+                required: true
+            },
+            name: {
+                required: true,
+                minlength: 2
+            }
+        },
+        messages: {
+            email: {
+                required: "Por favor, insira seu endereço de e-mail.",
+                email: "Por favor, insira um e-mail válido."
+            },
+            tel: {
+                required: "Por favor, insira seu número de telefone."
+            },
+            name: {
+                required: "Por favor, insira seu nome completo.",
+                minlength: "Seu nome deve ter pelo menos 2 caracteres."
+            }
+        }
+    });
 })
